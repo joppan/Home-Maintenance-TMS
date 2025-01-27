37b4af09f91f7066a687552f6e01e40f4110563f
Joseph Thomas <jomas97@gmail.com>
2025-01-27 15:20:25 -0700

    updating pom

--- src/main/java/com/valarkot/tms/config/AwsProperties.java
+++ src/main/java/com/valarkot/tms/config/AwsProperties.java
@@ -1,26 +1,26 @@
-package com.valarkot.tms.config;
+// package com.valarkot.tms.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import lombok.Data;
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.context.annotation.Configuration;
+// import lombok.Data;
 
-@Data
-@Configuration
-@ConfigurationProperties(prefix = "aws")
-public class AwsProperties {
+// @Data
+// @Configuration
+// @ConfigurationProperties(prefix = "aws")
+// public class AwsProperties {
     
-    private Credentials credentials = new Credentials();
-    private String region;
-    private DynamoDB dynamodb = new DynamoDB();
+//     private Credentials credentials = new Credentials();
+//     private String region;
+//     private DynamoDB dynamodb = new DynamoDB();
 
-    @Data
-    public static class Credentials {
-        private String accessKey;
-        private String secretKey;
-    }
+//     @Data
+//     public static class Credentials {
+//         private String accessKey;
+//         private String secretKey;
+//     }
 
-    @Data
-    public static class DynamoDB {
-        private String endpoint;
-    }
-}
+//     @Data
+//     public static class DynamoDB {
+//         private String endpoint;
+//     }
+// }
